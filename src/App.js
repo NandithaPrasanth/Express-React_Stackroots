09516174b7d48caf366e8277680bc7f243d237ff
Nanditha Unnikrishnan <nandithaprasanthu@gmail.com>
2022-02-25 14:23:33 +0530

    client folder is added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect,useState} from 'react'
+import{ Route,Routes,Navigate} from 'react-router-dom'
+import Main from './components/Main'
+import Signup from './components/Signup'
+import Login from './components/Login'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   const [backendData,setBackendData]=useState([{}])
+//   useEffect(()=>{
+//     fetch("/api").then(response=>response.json()).then(
+      
+//       data=> {setBackendData(data)}
+//     )
+//   },[])
+//   return (
+//     <div>
+//      {(typeof backendData.users === 'undefined') ? (
+//        <p>Loading...</p>
+//      ) : (
+//       //  backendData.users.map((user,i)=>{
+//       //    (<p key={i}> {user}</p>)
+//       backendData.users.map((user,i)=>(
+//         <p key={i}>{user}</p>
+//       ))
+       
+//      ) }
+//     </div>
+//   )
+// }
+
+function App(){
+  const user =localStorage.getItem("token")
+  return(
+    <Routes>
+     { user && <Route path='/' exact element={<Main/>}/>}
+     <Route path='/signup'exact element={<Signup/>}/>
+     <Route path='/login'exact element={<Login/>}/>
+     <Route path='/' exact element={<Navigate replace to='/login'/>}/>
+    </Routes>
+  )
 }
-
-export default App;
+export default App
